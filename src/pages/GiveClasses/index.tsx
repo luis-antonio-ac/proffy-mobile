45b8ba52c606248684b2c15eea268bcddf5a9f47
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-08-09 18:40:05 -0500

    Aplicativo mobile da aplicacao

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, ImageBackground, Text, Image } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+
+import { styles } from './styles'
+import bgImage from '../../assets/images/give-classes-background.png'
+
+function GiveClasses() {
+    const navigation = useNavigation()
+
+    function handleNavigateBack() {
+        navigation.goBack()
+    }
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={bgImage} resizeMode="contain" style={styles.content}>
+                <Text style={styles.title}>Quer ser um Proffy?</Text>
+                <Text style={styles.description}>Para começar, você precisa se cadastrar como professor em nossa plataforma web</Text>
+            </ImageBackground>
+
+            <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+                <Text style={styles.okButtonText}>Tudo bem</Text>
+            </RectButton>
+        </View>
+    )
+}
+
+export { GiveClasses }
